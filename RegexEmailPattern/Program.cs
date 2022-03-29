bbc49b739dda94f1ed5cbab22697dfe472896c6e
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-30 00:39:00 +0530

    added empty project file with welcome message

---
+++ RegexEmailPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexEmailPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Regex Email Pattern Problem.\n");
+        }
+    }
+}
