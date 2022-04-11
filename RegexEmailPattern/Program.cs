3f7afa8fc890de2fbe10c02096945ce02b5f7d0a
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-12 00:10:06 +0530

    refactored the code for the user Interface of the output

--- RegexEmailPattern/Program.cs
+++ RegexEmailPattern/Program.cs
@@ -6,11 +6,17 @@ namespace RegexEmailPattern
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to the Regex Email Pattern Problem.\n");
+            Console.WriteLine("\tWelcome to the Regex Email Pattern Problem");
+            Console.WriteLine("============================================================\n");
 
+            Console.WriteLine("   Custom Email Validation:");
+            Console.WriteLine("==============================\n");
             Console.Write("Enter email: ");
             string email = Console.ReadLine();
-            Console.WriteLine($"\nThe entered input i.e. {email} is " + Pattern.ValidateEmail(email));
+            if(Pattern.ValidateEmail(email))
+                Console.WriteLine($"\nThe entered input i.e. {email} is Valid.");
+            else
+                Console.WriteLine($"\nThe entered input i.e. {email} is InValid.");
         }
     }
 }
